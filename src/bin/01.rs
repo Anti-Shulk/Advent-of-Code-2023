8e364c8dda59c8d633ab8d1d0b6fa2efffa083fd
Justin Schwerdtfeger <justinschwerdtfeger245@gmail.com>
2023-12-01 16:06:38 -0600

    Finished day 1

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,11 +1,81 @@
+use std::collections::HashMap;
+
 advent_of_code::solution!(1);
 
 pub fn part_one(input: &str) -> Option<u32> {
-    None
+    Some(
+        input
+            .lines()
+            .map(|line| {
+                (
+                    line.chars().find(|s| s.is_numeric()),
+                    line.chars().rev().find(|s| s.is_numeric()),
+                )
+            })
+            .map(|(a, b)| {
+                format!("{}{}", a.unwrap(), b.unwrap())
+                    .parse::<u32>()
+                    .unwrap()
+            })
+            .sum(),
+    )
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let map: HashMap<&str, u32> = HashMap::from([
+        ("1", 1),
+        ("2", 2),
+        ("3", 3),
+        ("4", 4),
+        ("5", 5),
+        ("6", 6),
+        ("7", 7),
+        ("8", 8),
+        ("9", 9),
+        ("one", 1),
+        ("two", 2),
+        ("three", 3),
+        ("four", 4),
+        ("five", 5),
+        ("six", 6),
+        ("seven", 7),
+        ("eight", 8),
+        ("nine", 9),
+    ]);
+
+    Some(
+        input
+            .lines()
+            .map(|line| {
+                (
+                    map.iter()
+                        .filter_map(|(key, value)| line.find(key).map(|index| (index, value)))
+                        .fold(
+                            (10000_usize, &0_u32),
+                            |(acc_index, acc_value), (index, value)| {
+                                if index <= acc_index {
+                                    (index, value)
+                                } else {
+                                    (acc_index, acc_value)
+                                }
+                            },
+                        )
+                        .1,
+                    map.iter()
+                        .filter_map(|(key, value)| line.rfind(key).map(|index| (index, value)))
+                        .fold((0, &0_u32), |(acc_index, acc_value), (index, value)| {
+                            if index >= acc_index {
+                                (index, value)
+                            } else {
+                                (acc_index, acc_value)
+                            }
+                        })
+                        .1,
+                )
+            })
+            .map(|(a, b)| format!("{a}{b}").parse::<u32>().unwrap())
+            .sum(),
+    )
 }
 
 #[cfg(test)]
